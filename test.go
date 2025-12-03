581b785de60c8e757e65b593d9d57bd4ea237f3e
Ivan Lopez <ivlopez.dev@gmail.com>
2025-12-03 16:24:52 -0700

    [Refactor] Initial framework approach implementation

--- test.go
+++ test.go
@@ -1,26 +1,75 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	//"testing"
 	"io"
-	"log"
 	"net/http"
 )
 
-func main() {
-	res, err := http.Get("https://www.google.com/robots.txt")
+type Response struct {
+	StatusCode int
+	Body       string
+	Headers    http.Header
+}
+
+type APIClient struct {
+	BaseURL string
+	Client  *http.Client
+}
+
+type RequestOption struct {
+	Endpoint string
+	Headers  map[string]string
+	Payload  []byte
+}
+
+func (c *APIClient) Send(method string, opts RequestOption) (*Response, error) {
+	req, err := http.NewRequest(method, opts.Endpoint, bytes.NewBuffer(opts.Payload))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	body, err := io.ReadAll(res.Body)
-	res.Body.Close()
-	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	// Add Headers
+	for k, v := range opts.Headers {
+		req.Header.Set(k, v)
 	}
+
+	// Send Request
+	res, err := c.Client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Printf("%s", body)
+	defer res.Body.Close()
+
+	// Read Response Body
+	bodyBytes, _ := io.ReadAll(res.Body)
+
+	fmt.Println("Response Status:", res.Status)
+	return &Response{
+		StatusCode: res.StatusCode,
+		Body:       string(bodyBytes),
+		Headers:    res.Header,
+	}, nil
+}
+
+func main() {
+	client := &APIClient{
+		BaseURL: "https://swapi.dev/api",
+		Client:  &http.Client{},
+	}
+
+	opts := RequestOption{
+		Endpoint: client.BaseURL + "/people/1",
+		Headers:  map[string]string{"Content-Type": "application/json"},
+		// Payload:  []byte(``),
+	}
+
+	response, err := client.Send("GET", opts)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Response Body:", response.Body)
 }
